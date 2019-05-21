6eb3960c52d176e039234d8903900ab6fb991410
outrorun <jaxad@ya.ru>
2019-05-21 20:49:02 +0300

    new

---
+++ 01/task05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ctype.h>
+
+using namespace std;
+
+int main()
+{
+char a;
+cin>>a;
+bool f=islower(a);
+cout<<f;
+
+return 0;
+}
---
+++ 01/task06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cin>>a;
+
+cout
+<<a/1.487<<" pounds"<<endl
+<<a/0.172<<" franks"<<endl
+<<a/0.584<<" marks"<<endl
+<<a/0.00955<<" yens"<<endl;
+
+return 0;
+}
